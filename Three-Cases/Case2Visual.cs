0848271076d79897df2686de70d7ed9825ba799a
Mads <mads.ditjo@gmail.com>
2022-02-28 14:56:38 +0100

    Ændringer til danseprogram. + operator overload

--- Three-Cases/Case2Visual.cs
+++ Three-Cases/Case2Visual.cs
@@ -7,53 +7,59 @@ using Three_cases_ClassLibrary;
 
 namespace Three_Cases
 {
-    public class Case2Visual
-    {
-        public static void Dans()
+
+        public class Case2Visual
         {
-            //Indtastning af navn på den første danser efterfulgta af point.
-            //disse bliver gemt i passende variabler.
-
-            string navn1, navn2, svar, kør = "1";
-            int point1, point2;
-
-            while (kør == "1")
+            public static void Dans()
             {
-                Case2Logic Danser_obj = new Case2Logic();
+                //Indtastning af navn på den første danser efterfulgta af point.
+                //disse bliver gemt i passende variabler.
 
-                //Danser 1: Navn og point.
-                Console.WriteLine();
-                Console.Write("Indtast danser 1 navn: ");
-                navn1 = Console.ReadLine();
-                Console.Write("Indtast danser 1 point: ");
-                point1 = Convert.ToInt32(Console.ReadLine());
+                string navn1, navn2, kør = "1";
+                int point1, point2;
 
-                //Danser 2: Navn og point
-                Console.WriteLine();
-                Console.Write("Indtast danser 2 navn: ");
-                navn2 = Console.ReadLine();
-                Console.Write("Indtast danser 2 point: ");
-                point2 = Convert.ToInt32(Console.ReadLine());
+                while (kør == "1")
+                {
 
-                //Mothoden er overloaded. Den har samme navn men gør noget forskelliget alt efter hvilket indput den for.
-                svar = Danser_obj.Dans(navn1, navn2);
-                svar = svar + Danser_obj.Dans(point1, point2);
+                    Console.Clear();
+                    //navn og point danser1
+                    Console.WriteLine();
+                    Console.Write("Indtast danser 1 navn: ");
+                    navn1 = Console.ReadLine();
+                    Console.Write("Indtast danser 1 point: ");
+                    point1 = Convert.ToInt32(Console.ReadLine());
 
-                //svaret bliver udskrevet.
-                Console.WriteLine(svar);
+                    //Navn og point danser2
+                    Console.WriteLine();
+                    Console.Write("Indtast danser 2 navn: ");
+                    navn2 = Console.ReadLine();
+                    Console.Write("Indtast danser 2 point: ");
+                    point2 = Convert.ToInt32(Console.ReadLine());
+                    
+                    //informationen bliver lagt i vær deres objekt
+                    Dancepoint danser1 = new Dancepoint(navn1, point1);
+                    Dancepoint danser2 = new Dancepoint(navn2, point2);
 
-                Console.ReadKey();
+                    //ved at lægge danser1 og danser2 sammen bruger den den overloaded operator + i logiken
+                    Dancepoint resultat = danser1 + danser2;
+                    
+                    //resultatet bliver udskrevet her ved at kalde på de navn og point.
+                    Console.WriteLine(resultat.Navn + resultat.Point);
+                    
+                    Console.ReadKey();
 
-                //kør igen?
-                Console.Clear();
-                Console.WriteLine();
-                Console.WriteLine("Kør igen?");
-                Console.WriteLine();
-                Console.WriteLine("1: Kør programmet igen");
-                Console.WriteLine("2: Afslut program");
-                kør = Console.ReadLine();
+                    //kør igen?
+                    Console.Clear();
+                    Console.WriteLine();
+                    Console.WriteLine("Kør igen?");
+                    Console.WriteLine();
+                    Console.WriteLine("1: Kør programmet igen");
+                    Console.WriteLine("2: Afslut program");
+                    kør = Console.ReadLine();
+                }
             }
-        }
-    }
 
+        } 
 }
+
+
--- Three-cases ClassLibrary/Case2Logic.cs
+++ Three-cases ClassLibrary/Case2Logic.cs
@@ -6,21 +6,34 @@ using System.Threading.Tasks;
 
 namespace Three_cases_ClassLibrary
 {
-    public class Case2Logic
+    public class Dancepoint
     {
+        //der bliver lavet 2 fields: Navn og Point
+        public string Navn;
+        public int Point;
 
-        public string Dans(string navn1, string navn2)
-            //navnende bliver lagt sammen i en string
+        public Dancepoint(string navn, int point)
+        //fieldsne bliver sat i forbindelse med strings
         {
-            return navn1 + " & " + navn2 + " ";
+            Navn = navn;
+            Point = point;
         }
-        public int Dans(int point1, int point2)
-            //point bliver lagt sammen.a
+        public static Dancepoint operator +(Dancepoint danser1, Dancepoint danser2)
+        //operator + bliver overloaded. Når man lægger danser1 og danser2 sammen så gør den det som sker nedenunder.
         {
-            return point1 + point2;
+            //Navn bliver lagt sammen til en string ved brug af "string interpolation operator($)"
+            string resultatNavn = $"{danser1.Navn} & {danser2.Navn} ";
+
+            //Point bliver lagt sammen som man ved matematik
+            int resultatPoint = danser1.Point + danser2.Point;
+
+            //det nye objekt bliver fyldt med informationen ovenfra
+            Dancepoint resultat = new Dancepoint(resultatNavn, resultatPoint);
+
+            //og returneret
+            return resultat;
+
         }
-
-
     }
 }
 
